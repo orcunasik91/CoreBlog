8c0ef9d68c85c14cd505a40789b822e06c334bcb
Orçun Aşık <asikorcun@gmail.com>
2024-12-23 19:38:22 +0300

    Added WebUI project to Solution

---
+++ CoreBlog.WebUI/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CoreBlog.WebUI.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
