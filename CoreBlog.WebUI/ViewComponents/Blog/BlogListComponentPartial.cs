edfd34b7ec43cf4d430905aab05d668787838ae1
Orçun Aşık <asikorcun@gmail.com>
2024-12-25 20:48:07 +0300

    Blog Index'te kullanılacak 'BlogList' added viewcomponent class and page

---
+++ CoreBlog.WebUI/ViewComponents/Blog/BlogListComponentPartial.cs
@@ -0,0 +1,19 @@
+﻿using CoreBlog.Business.Abstract;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoreBlog.WebUI.ViewComponents.Blog;
+public class BlogListComponentPartial : ViewComponent
+{
+    private readonly IBlogService _blogService;
+
+    public BlogListComponentPartial(IBlogService blogService)
+    {
+        _blogService = blogService;
+    }
+
+    public IViewComponentResult Invoke()
+    {
+        var blogs = _blogService.GetList();
+        return View(blogs);
+    }
+}
