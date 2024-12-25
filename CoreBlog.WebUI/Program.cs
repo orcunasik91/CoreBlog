7e34e58528c5118c2ff07119e516847bbebc5146
Orçun Aşık <asikorcun@gmail.com>
2024-12-25 20:46:05 +0300

    DbContext, Blog service registrations were added to IoC

--- CoreBlog.WebUI/Program.cs
+++ CoreBlog.WebUI/Program.cs
@@ -1,21 +1,25 @@
+using CoreBlog.Business.Abstract;
+using CoreBlog.Business.Concrete;
+using CoreBlog.DataAccess.Abstract;
+using CoreBlog.DataAccess.Concrete.Context;
+using CoreBlog.DataAccess.Concrete.Repositories.EntityFramework;
+
 namespace CoreBlog.WebUI
 {
     public class Program
     {
         public static void Main(string[] args)
         {
-            var builder = WebApplication.CreateBuilder(args);
-
-            // Add services to the container.
+            WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
             builder.Services.AddControllersWithViews();
+            builder.Services.AddDbContext<MyAppContext>();
+            builder.Services.AddScoped<IBlogDal, EfBlogRepository>();
+            builder.Services.AddScoped<IBlogService, BlogManager>();
 
-            var app = builder.Build();
-
-            // Configure the HTTP request pipeline.
+            WebApplication app = builder.Build();
             if (!app.Environment.IsDevelopment())
             {
                 app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
 
@@ -28,7 +32,7 @@ namespace CoreBlog.WebUI
 
             app.MapControllerRoute(
                 name: "default",
-                pattern: "{controller=Home}/{action=Index}/{id?}");
+                pattern: "{controller=Blog}/{action=Index}/{id?}");
 
             app.Run();
         }
