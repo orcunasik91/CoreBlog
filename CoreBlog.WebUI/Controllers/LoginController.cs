e4a584c05a951830dc4177602db50840f1ddfb6d
Orçun Aşık <asikorcun@gmail.com>
2024-12-31 21:23:43 +0300

    Created Login page to Writer

---
+++ CoreBlog.WebUI/Controllers/LoginController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CoreBlog.WebUI.Controllers;
+public class LoginController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
