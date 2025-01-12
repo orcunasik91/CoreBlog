1fa39cfe015844e209db95d77ed7922668132752
Orçun Aşık <asikorcun@gmail.com>
2025-01-12 23:06:08 +0300

    WriterPanelNavbarSection edited

---
+++ CoreBlog.WebUI/ViewComponents/WriterPanelLayout/WriterPanelMessageNotificationSection.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CoreBlog.WebUI.ViewComponents.WriterPanelLayout;
+public class WriterPanelMessageNotificationSection : ViewComponent
+{
+    public IViewComponentResult Invoke()
+    {
+        return View();
+    }
+}
---
+++ CoreBlog.WebUI/ViewComponents/WriterPanelLayout/WriterPanelNotificationSection.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CoreBlog.WebUI.ViewComponents.WriterPanelLayout;
+public class WriterPanelNotificationSection : ViewComponent
+{
+    public IViewComponentResult Invoke()
+    {
+        return View();
+    }
+}
